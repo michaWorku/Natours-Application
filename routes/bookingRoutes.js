b29416d82cdcc898d1304cbd60e64ddcb6c5de77
michaworku <mikias27worku@gmail.com>
2022-04-05 11:44:29 +0300

    updating booking routes and controllers

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -1,21 +1,22 @@
-const express = require("express");
-const bookingController = require("./../controller/bookingController");
-const authController = require("./../controller/authController");
+const express = require('express');
+const bookingController = require('./../controllers/bookingController');
+const authController = require('./../controllers/authController');
 
 const router = express.Router();
+
 router.use(authController.protect);
 
-router.get("/checkout-session/:tourId", bookingController.getCheckoutSession);
+router.get('/checkout-session/:tourId', bookingController.getCheckoutSession);
 
-router.use(authController.restrictTo("admin", "lead-guide"));
+router.use(authController.restrictTo('admin', 'lead-guide'));
 
 router
-  .route("/")
-  .get(bookingController.getAllBooking)
+  .route('/')
+  .get(bookingController.getAllBookings)
   .post(bookingController.createBooking);
 
 router
-  .route("/:id")
+  .route('/:id')
   .get(bookingController.getBooking)
   .patch(bookingController.updateBooking)
   .delete(bookingController.deleteBooking);
