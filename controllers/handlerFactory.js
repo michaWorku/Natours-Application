1f54781aa7ef80215943ff96924b424070e10c11
michaworku <mikias27worku@gmail.com>
2022-04-06 11:24:16 +0300

    error fixed

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -1,17 +1,17 @@
-const AppError = require("../utils/appError");
-const catchAsync = require("../utils/catchAsync");
-const APIFeatures = require("../utils/apiFeatures");
+const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
+const APIFeatures = require('./../utils/apiFeatures');
 
-exports.delteOne = Model =>
+exports.deleteOne = Model =>
   catchAsync(async (req, res, next) => {
     const doc = await Model.findByIdAndDelete(req.params.id);
 
     if (!doc) {
-      return next(new AppError("No document found with that ID", 404));
+      return next(new AppError('No document found with that ID', 404));
     }
 
     res.status(204).json({
-      status: "success",
+      status: 'success',
       data: null
     });
   });
@@ -24,11 +24,11 @@ exports.updateOne = Model =>
     });
 
     if (!doc) {
-      return next(new AppError("No document found with that ID", 404));
+      return next(new AppError('No document found with that ID', 404));
     }
 
     res.status(200).json({
-      status: "success",
+      status: 'success',
       data: {
         data: doc
       }
@@ -37,11 +37,10 @@ exports.updateOne = Model =>
 
 exports.createOne = Model =>
   catchAsync(async (req, res, next) => {
-    console.log(req.body);
     const doc = await Model.create(req.body);
 
     res.status(201).json({
-      status: "success",
+      status: 'success',
       data: {
         data: doc
       }
@@ -55,11 +54,11 @@ exports.getOne = (Model, popOptions) =>
     const doc = await query;
 
     if (!doc) {
-      return next(new AppError("No document found with that ID", 404));
+      return next(new AppError('No document found with that ID', 404));
     }
 
     res.status(200).json({
-      status: "success",
+      status: 'success',
       data: {
         data: doc
       }
@@ -68,25 +67,24 @@ exports.getOne = (Model, popOptions) =>
 
 exports.getAll = Model =>
   catchAsync(async (req, res, next) => {
-    // EXECUTE QUERY
-
-    // To allow for nested GET reviews on tour
+    // To allow for nested GET reviews on tour (hack)
     let filter = {};
     if (req.params.tourId) filter = { tour: req.params.tourId };
 
     const features = new APIFeatures(Model.find(filter), req.query)
       .filter()
       .sort()
-      .limitFilds()
+      .limitFields()
       .paginate();
-    const doc = await features.query;
     // const doc = await features.query.explain();
+    const doc = await features.query;
+
     // SEND RESPONSE
     res.status(200).json({
-      status: "success",
+      status: 'success',
       results: doc.length,
-      data: { 
-        data: doc 
+      data: {
+        data: doc
       }
     });
   });
