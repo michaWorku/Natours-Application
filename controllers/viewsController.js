f18ac58be009f007ded41d2deb68e2601ad32a42
michaworku <mikias27worku@gmail.com>
2022-04-06 10:25:01 +0300

    refactring views routes and controllers

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,25 +1,17 @@
-const Tour = require('../models/tourModel');
-const User = require('../models/userModel');
-const Booking = require('../models/bookingModel');
-const catchAsync = require('../utils/catchAsync');
-const AppError = require('../utils/appError');
-
-exports.alerts = (req, res, next) => {
-  const { alert } = req.query;
-  if (alert === 'booking')
-    res.locals.alert =
-      "Your booking was successful! Please check your email for a confirmation. If your booking doesn't show up here immediatly, please come back later.";
-  next();
-};
+const Tour = require("../models/tourModel");
+const User = require("../models/userModel");
+const Booking = require("../models/bookingModel");
+const AppError = require("../utils/appError");
+const catchAsync = require("../utils/catchAsync");
 
 exports.getOverview = catchAsync(async (req, res, next) => {
-  // 1) Get tour data from collection
+  // 1) Get Tour data from collections
   const tours = await Tour.find();
 
-  // 2) Build template
-  // 3) Render that template using tour data from 1)
-  res.status(200).render('overview', {
-    title: 'All Tours',
+  // 2) Build Template
+  // 3) Render template using tour data
+  res.status(200).render("overview", {
+    title: "All Tour",
     tours
   });
 });
@@ -27,31 +19,42 @@ exports.getOverview = catchAsync(async (req, res, next) => {
 exports.getTour = catchAsync(async (req, res, next) => {
   // 1) Get the data, for the requested tour (including reviews and guides)
   const tour = await Tour.findOne({ slug: req.params.slug }).populate({
-    path: 'reviews',
-    fields: 'review rating user'
+    path: "reviews",
+    fields: "review rating user"
   });
 
-  if (!tour) {
-    return next(new AppError('There is no tour with that name.', 404));
-  }
+  // Handling error
+  if (!tour) return next(new AppError("There is no tour with that name.", 404));
 
-  // 2) Build template
-  // 3) Render template using data from 1)
-  res.status(200).render('tour', {
+  // 2) Build Template
+  // 3) Render template using tour data
+  res.status(200).render("tour", {
     title: `${tour.name} Tour`,
     tour
   });
 });
 
 exports.getLoginForm = (req, res) => {
-  res.status(200).render('login', {
-    title: 'Log into your account'
+  res.status(200).render("login", {
+    title: "Log into your account"
+  });
+};
+
+exports.getSignupForm = (req, res) => {
+  res.status(200).render("signup", {
+    title: "Sign up your account"
   });
 };
 
 exports.getAccount = (req, res) => {
-  res.status(200).render('account', {
-    title: 'Your account'
+  res.status(200).render("account", {
+    title: "Your account"
+  });
+};
+
+exports.getForgetPasswordForm = (req, res) => {
+  res.status(200).render("forgotPassword", {
+    title: "Forgot your password"
   });
 };
 
@@ -59,31 +62,27 @@ exports.getMyTours = catchAsync(async (req, res, next) => {
   // 1) Find all bookings
   const bookings = await Booking.find({ user: req.user.id });
 
-  // 2) Find tours with the returned IDs
-  const tourIDs = bookings.map(el => el.tour);
-  const tours = await Tour.find({ _id: { $in: tourIDs } });
+  // 2) Find tours with the returned IDS
+  const tourIDS = bookings.map(el => el.tour);
+  const tours = await Tour.find({ _id: { $in: tourIDS } });
 
-  res.status(200).render('overview', {
-    title: 'My Tours',
+  res.status(200).render("overview", {
+    title: "My Tours",
     tours
   });
 });
 
 exports.updateUserData = catchAsync(async (req, res, next) => {
-  const updatedUser = await User.findByIdAndUpdate(
+  const updateUser = await User.findByIdAndUpdate(
     req.user.id,
-    {
-      name: req.body.name,
-      email: req.body.email
-    },
+    { name: req.body.name, email: req.body.email },
     {
       new: true,
       runValidators: true
     }
   );
-
-  res.status(200).render('account', {
-    title: 'Your account',
-    user: updatedUser
+  res.status(200).render("account", {
+    title: "Your account",
+    user: updateUser
   });
 });
